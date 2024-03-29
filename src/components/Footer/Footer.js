ce90887a29fb505bc5d08c12ca2e97044b8f9587
VanceTechwell <matthewkenn00@gmail.com>
2024-03-29 19:07:43 +0700

    Update author name and project details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/AxelAlinsky'
+      href='https://github.com/VanceTechwell'
       className='link footer__link'
     >
-      Created By Alex
+      Created By Vance
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://github.com/AxelAlinsky',
-  title: '.A',
+  homepage: 'https://github.com/VanceTechwell',
+  title: '.V',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'Alex',
+  name: 'Vance',
   role: 'QA Automation Engineer',
   description:
     'A seasoned Senior QA Automation Engineer located in Bangkok with extensive experience in guiding sophisticated automation approaches. My proficiency in leveraging Python, TypeScript, Robot Framework, Selenium, and Playwright empowers me to craft and sustain cutting-edge testing frameworks and CI/CD pipelines, significantly enhancing operational efficiency and product quality across diverse tech industries. ',
-  resume: 'https://raw.githubusercontent.com/AxelAlinsky/alexautomation/feat/add-resume/public/resume/Alex.pdf',
+  resume: 'https://raw.githubusercontent.com/VanceTechwell/Vanceautomation/feat/add-resume/public/resume/Vance.pdf',
   social: {
     linkedin: 'https://www.linkedin.com/in/mattapichai/',
-    github: 'https://github.com/AxelAlinsky',
+    github: 'https://github.com/VanceTechwell',
   },
 }
 
@@ -22,21 +22,21 @@ const projects = [
   // if there are no projects, Projects section won't show up
   {
     name: 'Mocha Demo Automation',
-    imageUrl: 'https://raw.githubusercontent.com/AxelAlinsky/alexautomation/main/public/image/mocha-automation.png',
+    imageUrl: 'https://raw.githubusercontent.com/VanceTechwell/Vanceautomation/main/public/image/mocha-automation.png',
     description:
       `The automation framework utilizes Mocha with TypeScript to create efficient and reliable tests for the SauceDemo website. Leveraging TypeScript's static typing and Mocha's testing capabilities, the framework ensures organized, modular tests with robust assertions and detailed reporting, enhancing maintainability and test coverage.`,
     stack: ['Mocha', 'TypeScript'],
-    sourceCode: 'https://github.com/AxelAlinsky/mocha-workspace-automation',
-    livePreview: 'https://github.com/AxelAlinsky/mocha-workspace-automation',
+    sourceCode: 'https://github.com/VanceTechwell/mocha-workspace-automation',
+    livePreview: 'https://github.com/VanceTechwell/mocha-workspace-automation',
   },
   {
     name: 'Playwright Demo Automation',
-    imageUrl: 'https://raw.githubusercontent.com/AxelAlinsky/alexautomation/main/public/image/playwright-automation.png',
+    imageUrl: 'https://raw.githubusercontent.com/VanceTechwell/Vanceautomation/main/public/image/playwright-automation.png',
     description:
       `This automation system combines TypeScript and Playwright to automate tasks on the SauceDemo website. TypeScript helps ensure code reliability, while Playwright simplifies browser automation. Together, they enable smooth interaction with the website's elements, making it easier to create and maintain tests. This setup ensures that testing the SauceDemo website is efficient and effective.`,
     stack: ['Playwright', 'TypeScript'],
-    sourceCode: 'https://github.com/AxelAlinsky/playwright-workspace-automation',
-    livePreview: 'https://github.com/AxelAlinsky/mocha-workspace-automation',
+    sourceCode: 'https://github.com/VanceTechwell/playwright-workspace-automation',
+    livePreview: 'https://github.com/VanceTechwell/mocha-workspace-automation',
   },
 ]
 
